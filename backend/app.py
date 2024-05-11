65447f45fe2b0bd19dad932a23af097353ab3f08
Manuel Bilbao <manuel.bilbao@lambdaclass.com>
2024-05-10 21:48:07 -0300

    Add character endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_cors import CORS
 
-from db.characters import get_all_characters
+from time import sleep
+
+from db.characters import get_all_characters, get_character_by_id
 
 app = Flask(__name__)
 CORS(app)
@@ -9,3 +11,8 @@ CORS(app)
 @app.route("/characters")
 def all_characters():
     return get_all_characters()
+
+@app.route("/characters/<id>")
+def character_by_id(id):
+    sleep(1)
+    return get_character_by_id(id)
