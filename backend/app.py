85b66bdcc784d5a0b76b7122a19be2bbc298441b
Manuel Bilbao <manuel.bilbao@lambdaclass.com>
2024-05-13 20:56:34 -0300

    Add DELETE endpoint for characters

--- backend/app.py
+++ backend/app.py
@@ -3,10 +3,21 @@ from flask_cors import CORS
 
 from time import sleep
 
-from db.characters import get_all_characters, get_character_by_id
+from db.characters import get_all_characters, get_character_by_id, remove_character
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route("/")
+def home():
+    return """
+    <html>
+    <body>
+    <h1>Welcome to my heroes API</h1>
+    <a href="/characters">Go to all characters</a>
+    </body>
+    </html>
+    """
 
 @app.route("/characters")
 def all_characters():
@@ -16,3 +27,7 @@ def all_characters():
 def character_by_id(id):
     sleep(1)
     return get_character_by_id(id)
+
+@app.route("/characters/<id>", methods = ["DELETE"])
+def remove_character_by_id(id):
+    return {"success": remove_character(id)}
