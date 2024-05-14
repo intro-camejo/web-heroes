80fecd0668ec1b5af6c7788a7e724a2cd8ffb582
Manuel Bilbao <manuel.bilbao@lambdaclass.com>
2024-05-13 22:03:20 -0300

    Add POST endpoint to create characters

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 
 from time import sleep
 
-from db.characters import get_all_characters, get_character_by_id, remove_character
+from db.characters import add_character, get_all_characters, get_character_by_id, remove_character
 
 app = Flask(__name__)
 CORS(app)
@@ -31,3 +31,27 @@ def character_by_id(id):
 @app.route("/characters/<id>", methods = ["DELETE"])
 def remove_character_by_id(id):
     return {"success": remove_character(id)}
+
+@app.route("/characters", methods = ["POST"])
+def create_character():
+    name = request.json.get("name")
+    names = request.json.get("names")
+    publisher = request.json.get("publisher")
+    gender = request.json.get("gender")
+    alignment = request.json.get("alignment")
+    image = request.json.get("image")
+    race = request.json.get("race")
+
+    id = int(get_all_characters()[-1]["id"]) + 1
+
+    character = {
+        "id": id,
+        "name": name,
+        "names": names,
+        "publisher": publisher,
+        "gender": gender,
+        "alignment": alignment,
+        "image": image,
+        "race": race
+    }
+    return {"success": add_character(character), "id": id}
